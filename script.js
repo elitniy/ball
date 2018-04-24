2ade613ced48e16e43b435e4da4933fe7f5d1d05
P206 <elitniy.07@gmail.com>
2018-04-24 14:10:51 +0400

    ispravleniye owibki border

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 var billiards = document.querySelector(".billiards");
 var ball = document.querySelector(".ball");
 var topp = 1;
-var left = 1;
+var leftt = 1;
 var x= 1;
 var y= 1;
 var winWidth = billiards.offsetWidth;
 var winHeight = billiards.offsetHeight;
 var count = 0;
- function gooo(){
- 	if (topp == winHeight -30) {
- 		y = y * (-1);
- 	};
- 	if (left == winWidth - 30) {
- 		x = x * (-1);
- 	};
- 	if (topp == 0) {
- 		y = y * (-1);
- 	};
- 	if (left == 0) {
- 		x = x * (-1);
- 	};
+  function gooo(){
+ 	  if (topp == winHeight -30) {
+ 		 y = y * (-1);
+ 	  };
+ 	  if (leftt == winWidth - 30) {
+ 		 x = x * (-1);
+ 	  };
+ 	  if (topp == 0) {
+ 		 y = y * (-1);
+ 	  };
+ 	  if (leftt == 0) {
+ 		 x = x * (-1);
+ 	  };
 	topp = topp + (1* y)
-	left = left +(1 * x) 
+	leftt = leftt +(1 * x) 
 	ball.style.top = topp +"px";
-	ball.style.left = left + "px";
+	ball.style.left = leftt + "px";
 }
 ball.onmousedown = function(e) {
 
@@ -37,7 +37,7 @@ ball.onmousedown = function(e) {
   	function moveAt(e) {
     	ball.style.left = (e.pageX - shiftX - winWidth/2) + "px";
     	ball.style.top = (e.pageY - shiftY - winHeight/2) + "px";
-      left = (e.pageX - shiftX - winWidth/2);
+      leftt = (e.pageX - shiftX - winWidth/2);
       topp = (e.pageY - shiftY - winHeight/2);
       
   	}
